dfb68b5fc66d505050421fd3fe5b19e4cd87acb4
Christoph Pader <chris@margelo.com>
2024-10-14 13:52:42 +0200

    show header

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -12,7 +12,6 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
       }}
     >
       <Tabs.Screen
