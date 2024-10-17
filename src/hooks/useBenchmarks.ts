888141aafc5e97a4fe22d9e7945675ec57f325e1
Christoph Pader <chris@margelo.com>
2024-10-17 11:21:47 +0200

    fix: pause times

--- src/hooks/useBenchmarks.ts
+++ src/hooks/useBenchmarks.ts
@@ -6,9 +6,14 @@ import {
   BenchmarkRunnerResult,
   BenchmarkRunnerResults,
   Benchmarks,
-  BENCHMARKS,
-} from "@/constants/Benchmarks";
+} from "@/constants/benchmarks/types";
+import { BENCHMARKS } from "@/constants/benchmarks";
 import { useCallback, useState } from "react";
+
+// We want to wait a few milliseconds between each benchmark, runner and iteration to give the system time to cool down
+const BENCHMARK_PAUSE_TIME = 1000;
+const BENCHMARK_RUNNER_PAUSE_TIME = 100;
+const BENCHMARK_RUNNER_ITERATION_PAUSE_TIME = 10;
 
 export function useBenchmarks() {
   const [results, setResults] = useState<BenchmarkResults>();
@@ -22,13 +27,76 @@ export function useBenchmarks() {
   return { results, startBenchmarks };
 }
 
+async function runBenchmarks(
+  benchmarks: Benchmarks
+): Promise<BenchmarkResults> {
+  console.log("--------- BEGINNING BENCHMARKS ---------");
+
+  const results = await executeInSequence(
+    Object.values(benchmarks),
+    async (benchmark) => runBenchmark(benchmark),
+    BENCHMARK_PAUSE_TIME
+  );
+
+  console.log();
+  console.log("--------- FINISHED SQLITE BENCHMARKS! ---------");
+
+  return Object.fromEntries(
+    results.map((result) => [result.id, result])
+  ) as BenchmarkResults;
+}
+
+async function runBenchmark(benchmark: Benchmark): Promise<BenchmarkResult> {
+  console.log();
+  console.log(`🐎 ${benchmark.description}`);
+  const results = await executeInSequence(
+    Object.values(benchmark.runners),
+    async (runner) => await executeBenchmarkRunner(benchmark, runner),
+    BENCHMARK_RUNNER_PAUSE_TIME
+  );
+  console.log("----------------------------------------");
+
+  const runnerResults = Object.fromEntries(
+    results.map((result) => [result.library, result])
+  ) as BenchmarkRunnerResults;
+
+  return { id: benchmark.id, runnerResults };
+}
+
+async function executeBenchmarkRunner(
+  benchmark: Benchmark,
+  runner: BenchmarkRunner,
+  pauseTime = BENCHMARK_RUNNER_ITERATION_PAUSE_TIME
+): Promise<BenchmarkRunnerResult> {
+  console.log(`🏗️  ${runner.library}: Preparing benchmark`);
+  runner.prepare?.();
+
+  console.log(`⏳ ${runner.library}: Running ${benchmark.numberOfRuns}x times`);
+
+  const times: number[] = [];
+  for (let i = 0; i < benchmark.numberOfRuns; i++) {
+    const start = performance.now();
+    await runner.run(i);
+    const end = performance.now();
+    times.push(end - start);
+
+    await wait(pauseTime);
+  }
+  const averageTime = times.reduce((a, b) => a + b, 0) / times.length;
+
+  console.log(`✅ ${runner.library}: Took ${averageTime.toFixed(2)}ms to run!`);
+  console.log();
+
+  return { library: runner.library, times, averageTime };
+}
+
 const wait = (ms: number) =>
   new Promise<void>((resolve) => setTimeout(resolve, ms));
 
 async function executeInSequence<Task, Result>(
   tasks: Task[],
   execute: (t: Task) => Promise<Result>,
-  pauseTime = 1000 // between each benchmark runner, we want to wait 1000ms by default
+  pauseTime = 0
 ) {
   const results: Result[] = [];
   async function run(i = 0): Promise<void> {
@@ -42,59 +110,4 @@ async function executeInSequence<Task, Result>(
 
   await run();
   return results;
-}
-
-async function executeBenchmarkRunner(
-  benchmark: Benchmark,
-  runner: BenchmarkRunner
-): Promise<BenchmarkRunnerResult> {
-  console.log(`🏗️  ${runner.library}: Preparing benchmark`);
-  runner.prepare?.();
-
-  console.log(`⏳ ${runner.library}: Running ${benchmark.numberOfRuns}x times`);
-  const start = performance.now();
-  for (let i = 0; i < benchmark.numberOfRuns; i++) await runner.run(i);
-  const end = performance.now();
-  const time = (end - start).toFixed(2);
-
-  console.log(`✅ ${runner.library}: Took ${time}ms to run!`);
-  console.log();
-
-  return { library: runner.library, time };
-}
-
-async function runBenchmark(benchmark: Benchmark): Promise<BenchmarkResult> {
-  console.log();
-  console.log(`🐎 ${benchmark.description}`);
-  const results = await executeInSequence(
-    Object.values(benchmark.runners),
-    async (runner) => await executeBenchmarkRunner(benchmark, runner),
-    // Between different benchmarks, we want to wait a bit longer
-    3000
-  );
-  console.log("----------------------------------------");
-
-  const runnerResults = Object.fromEntries(
-    results.map((result) => [result.library, result])
-  ) as BenchmarkRunnerResults;
-
-  return { id: benchmark.id, runnerResults };
-}
-
-async function runBenchmarks(
-  benchmarks: Benchmarks
-): Promise<BenchmarkResults> {
-  console.log("--------- BEGINNING BENCHMARKS ---------");
-
-  const results = await executeInSequence(
-    Object.values(benchmarks),
-    async (benchmark) => runBenchmark(benchmark)
-  );
-
-  console.log();
-  console.log("--------- FINISHED SQLITE BENCHMARKS! ---------");
-
-  return Object.fromEntries(
-    results.map((result) => [result.id, result])
-  ) as BenchmarkResults;
 }
